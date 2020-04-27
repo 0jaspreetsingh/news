670343340d383041d3f33b44d5e71a5500ef767d
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-04-27 21:30:07 +0530

    removed ip recognition

--- app.constants.ts
+++ app.constants.ts
@@ -3,5 +3,4 @@ export const config = {
 	nasaApiUrl: 'https://api.nasa.gov/',
 	newsApiKey: '7ecbb7b9d6d64bfaa50d1bf9779ebede',
 	nasaApiKey: 'cwQiNCYkgGL1PWMHXu4CifSdgZKhHytfDv5rnxop',
-	ipInfoUrl: 'http://ip-api.com/json'
 };
--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,25 +1,17 @@
-import { IpInfoService } from './../shared/service/ip-info/ip-info.service';
 import { Component, OnInit } from '@angular/core';
 import { StoryService } from '../shared/service/story/story.service';
 
 @Component({
-  selector: 'ne-story',
-  templateUrl: './story.component.html',
-  styleUrls: ['./story.component.css']
+	selector: 'ne-story',
+	templateUrl: './story.component.html',
+	styleUrls: [ './story.component.css' ]
 })
 export class StoryComponent implements OnInit {
+	constructor(private storyService: StoryService) {}
 
-  constructor(private storyService:StoryService,private ipInfoService:IpInfoService) { }
-
-  ngOnInit(): void {
-    this.storyService.getHeadlinesByCountry('us').subscribe(data=>{
-      console.log(data);
-    });
-    this.ipInfoService.getIpInfo().subscribe(data=>{
-      console.log(data);
-    });
-  }
-
-
-
+	ngOnInit(): void {
+		this.storyService.getHeadlinesByCountry('us').subscribe((data) => {
+			console.log(data);
+		});
+	}
 }
