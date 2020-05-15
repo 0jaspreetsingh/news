c0bcd00ea79a4e8bc5e52a427d6691edec831a0e
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-05-16 00:55:38 +0530

    created headlines ui

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
 	declarations: [],
-	imports: [ CommonModule, MatToolbarModule, MatIconModule ],
-	exports: [ MatToolbarModule, MatIconModule ]
+	imports: [ CommonModule, MatToolbarModule, MatIconModule, MatMenuModule ,MatButtonModule],
+	exports: [ MatToolbarModule, MatIconModule, MatMenuModule,MatButtonModule ]
 })
 export class AngularMaterialModule {}
--- src/app/shared/service/ipInfo/ip-info.service.ts
+++ src/app/shared/service/ipInfo/ip-info.service.ts
@@ -1,17 +1,29 @@
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { config } from 'app.constants';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class IpInfoService {
 	apiUrl: string = config.ipInfoUrl;
+
+	ipInfo: BehaviorSubject<any> = new BehaviorSubject<any>(undefined);
 
 	constructor(private http: HttpClient) {}
 
 	getIpInfo(): Observable<any> {
 		return this.http.get(this.apiUrl);
+	}
+
+	getipInfoSubject(): Observable<any> {
+		return this.ipInfo.asObservable();
+	}
+
+	setIpinfoSubject(data: any) {
+		this.ipInfo.next(data);
 	}
 }
