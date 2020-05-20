fd3133fe14190544e507e782204bd7305588192c
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-05-21 00:25:28 +0530

    added new services

--- src/app/shared/service/story/story.service.ts
+++ src/app/shared/service/story/story.service.ts
@@ -13,17 +13,17 @@ export class StoryService {
 
 	constructor(private http: HttpClient) {}
 
-	getHeadlinesByCountry(country: string): Observable<any> {
-		return this.http.get(`${this.apiUrl}top-headlines?country=${country}`, { headers: this.httpHeader });
-	}
+	// getHeadlinesByCountry(country: string): Observable<any> {
+	// 	return this.http.get(`${this.apiUrl}top-headlines?country=${country}`, { headers: this.httpHeader });
+	// }
 
-	getHeadlinesByCategory(country: string): Observable<any> {
-		return this.http.get(`${this.apiUrl}top-headlines?category=${country}`, { headers: this.httpHeader });
-	}
+	// getHeadlinesByCategory(country: string): Observable<any> {
+	// 	return this.http.get(`${this.apiUrl}top-headlines?category=${country}`, { headers: this.httpHeader });
+	// }
 
-	searchNews(query: string): Observable<any> {
-		return this.http.get(`${this.apiUrl}top-headlines?q=${query}`, { headers: this.httpHeader });
-	}
+	// searchNews(query: string): Observable<any> {
+	// 	return this.http.get(`${this.apiUrl}top-headlines?q=${query}`, { headers: this.httpHeader });
+	// }
 
 	/**
 	 * 
@@ -38,6 +38,56 @@ export class StoryService {
 		searchQuery = pageSize ? searchQuery + '&pageSize=' + pageSize : searchQuery;
 		searchQuery = page ? searchQuery + '&page=' + page : searchQuery;
 		return this.http.get(`${this.apiUrl}top-headlines?country=${country}${searchQuery}`, {
+			headers: this.httpHeader
+		});
+	}
+
+	/**
+	 * 
+	 * @param query 
+	 * @param domain 
+	 * @param excludeDomains 
+	 * @param from 
+	 * @param to 
+	 * @param language 
+	 * @param sortBy 
+	 * @param pageSize 
+	 * @param page 
+	 */
+	getEverything(
+		query: string,
+		sources?: string[],
+		domains?: string[],
+		excludeDomains?: string[],
+		from?: string,
+		to?: string,
+		language?: string,
+		sortBy?: string,
+		pageSize?: string,
+		page?: string
+	): Observable<any> {
+		query = query.replace(/\s/g, '+');
+		let searchQuery = '';
+		searchQuery = sources && sources.length > 0 ? searchQuery + '&sources=' + sources.toString() : searchQuery;
+		searchQuery = domains ? searchQuery + '&domains=' + domains.toString() : searchQuery;
+		searchQuery = excludeDomains ? searchQuery + '&excludeDomains=' + excludeDomains.toString() : searchQuery;
+		searchQuery = from ? searchQuery + '&from=' + from : searchQuery;
+		searchQuery = to ? searchQuery + '&to=' + to : searchQuery;
+		searchQuery = language ? searchQuery + '&language=' + language : searchQuery;
+		searchQuery = sortBy ? searchQuery + '&sortBy=' + sortBy : searchQuery;
+		searchQuery = pageSize ? searchQuery + '&pageSize=' + pageSize : searchQuery;
+		searchQuery = page ? searchQuery + '&page=' + page : searchQuery;
+
+		return this.http.get(`${this.apiUrl}everything?q=${query}${searchQuery}`, {
+			headers: this.httpHeader
+		});
+	}
+	getSources(category?: string, language?: string, country?: string): Observable<any> {
+		let searchQuery = '';
+		searchQuery = category ? searchQuery + '&category=' + category : searchQuery;
+		searchQuery = language ? searchQuery + '&language=' + language : searchQuery;
+		searchQuery = country ? searchQuery + '&country=' + country : searchQuery;
+		return this.http.get(`${this.apiUrl}sources${searchQuery}`, {
 			headers: this.httpHeader
 		});
 	}
