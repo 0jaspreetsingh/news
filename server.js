68994445bf5a73a90f68128f4cb619def6ce2840
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-07-04 19:52:37 +0530

    rollback

--- server.js
+++ server.js
@@ -3,33 +3,8 @@
 const express = require('express');
 const path = require('path');
 
-const cors = require('cors');
-
 const app = express();
 
-// add this code
-const whitelist = ['https://newsapi.org/v2/','http://ip-api.com/json','http://localhost:4201','http://news-anywhere.herokuapp.com/']; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            console.log('************');
-            console.log(origin);
-            console.log('************');
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/news'));
 
