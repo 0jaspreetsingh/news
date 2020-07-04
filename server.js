279e7cdc6cb7840fb7eb1135ed87c5b344bd8513
0jaspreetsingh <49236940+0jaspreetsingh@users.noreply.github.com>
2020-07-04 19:18:23 +0530

    trying to enable cors

--- server.js
+++ server.js
@@ -3,14 +3,30 @@
 const express = require('express');
 const path = require('path');
 
+const cors = require('cors');
+
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// add this code
+const whitelist = ['https://newsapi.org']; // list of allow domain
 
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/news'));
 
